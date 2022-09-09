53b7899abb2166e468b8d705d8131a405ede559f
Akash <adesigns>
2022-09-09 11:32:09 -0400

    added project media

--- src/components/Projects/Project/Project.jsx
+++ src/components/Projects/Project/Project.jsx
@@ -1,33 +1,8 @@
 import React from "react";
-import { Octokit } from "octokit";
 import { useEffect } from "react";
 import { useState } from "react";
 
 const Project = () => {
-
-    const [repoData, setRepoData] = useState([]);
-
-    const getRepos = async () => {
-        const octokit = new Octokit({
-            auth: process.env.REACT_APP_SECRET
-        });
-
-        try {
-            const data = await octokit.request("GET /users/{username}/repos", {
-                username: "adesignsm",
-            });
-
-            setRepoData(data);
-            console.log(repoData);
-
-        } catch (err) {
-            console.log(err);
-        }
-    }
-
-    useEffect(() => {
-        getRepos();
-    }, [repoData]);
 
     return (
         <>
